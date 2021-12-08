82f9716ad4203b7fd85649a6911037b45df4168d
Nathan E <nenns@ksu.edu>
2021-12-08 11:56:29 -0600

    another commit

---
+++ Flasher.py
@@ -0,0 +1,51 @@
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
+from PyQt5 import QtWidgets
+from Flash import Ui_MainWindow
+
+
+import RPi.GPIO as GPIO  # import GPIO
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.pushButton.pressed.connect(lambda: GPIO.output(40,1))
+
+        self.pushButton.released.connect(lambda: GPIO.output(40,0))
+
+        GPIO.add_event_detect(40, GPIO.BOTH, self.box)
+
+    # def on(self):
+    #     self.checkBox.setChecked(1)
+
+    # def off(self):
+    #     self.checkBox.setChecked(0)
+
+        try:
+            while True:
+                if GPIO.event_detected(40):
+                    
+                    if self.checkBox.checkState() == 1:
+                        self.checkBox.setChecked(0)
+
+                    else:
+                        self.checkBox.setChecked(1)
+
+                    
+        except KeyboardInterrupt:
+            pass
+
+        
+if __name__ == '__main__':
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(40, GPIO.OUT)
+
+    app = QApplication(sys.argv)
+    myapp = MainWindow()
+    myapp.show()
+    sys.exit(app.exec_())
